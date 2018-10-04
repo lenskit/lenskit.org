06038be086cbbe82a0c53a3677d854dbab253d3c
Michael Ekstrand <md@ekstrandom.net>
2018-10-04 12:18:05 -0700

    start the new LK website

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+"use strict";
+const path = require('path');
+const fs = require('fs');
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')();
+
+const args = require('minimist')(process.argv.slice(2));
+
+const outputDir = args['dest-dir'] || '_site';
+
+gulp.task('pages', function() {
+  const pages = require('./lib/pages');
+  return pages.render('content', outputDir, 'layouts');
+});
+
+gulp.task('styles', function() {
+  return gulp.src('styles/*.css')
+             .pipe($.ignore.exclude('_*.css'))
+             .pipe($.sourcemaps.init())
+             .pipe($.postcss([
+               require('postcss-nested'),
+               require('postcss-custom-properties')
+             ]))
+             .pipe($.sourcemaps.write('.'))
+             .pipe(gulp.dest(path.join(outputDir, 's')));
+});
+
+gulp.task('build', gulp.parallel('pages', 'styles'));
+
+gulp.task('default', gulp.series('build'));
+
+gulp.task('serve', function() {
+  var testServer = require('./lib/test-server');
+  testServer(outputDir);
+});
