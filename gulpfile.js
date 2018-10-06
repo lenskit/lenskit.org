00aef93c0324ff603ae6f82681427ad82abfb3ee
Michael Ekstrand <md@ekstrandom.net>
2018-10-06 09:51:13 -0700

    Getting started on the homepage

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,12 @@ const args = require('minimist')(process.argv.slice(2));
 
 const outputDir = args['dest-dir'] || '_site';
 
+gulp.task('static', function() {
+  return gulp.src('static/**')
+             .pipe($.changed(outputDir))
+             .pipe(gulp.dest(outputDir));
+})
+
 gulp.task('pages', function() {
   const pages = require('./lib/pages');
   return pages.render('content', outputDir, 'layouts');
@@ -18,6 +24,7 @@ gulp.task('styles', function() {
              .pipe($.ignore.exclude('_*.css'))
              .pipe($.sourcemaps.init())
              .pipe($.postcss([
+               require('postcss-custom-media'),
                require('postcss-nested'),
                require('postcss-custom-properties')
              ]))
@@ -25,7 +32,7 @@ gulp.task('styles', function() {
              .pipe(gulp.dest(path.join(outputDir, 's')));
 });
 
-gulp.task('build', gulp.parallel('pages', 'styles'));
+gulp.task('build', gulp.parallel('pages', 'static', 'styles'));
 
 gulp.task('default', gulp.series('build'));
 
